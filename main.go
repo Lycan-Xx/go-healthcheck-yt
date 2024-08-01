71080ade4b3c742020b5a89a88df48edb9ee0a6d
Lycan <msbello514@gmail.com>
2024-08-02 00:12:34 +0100

    Second commit

--- main.go
+++ main.go
@@ -8,30 +8,30 @@ import (
 )
 
 func main() {
-	app := &cli.app{
-		Name:"Healthchecker",
-		Usage:"A tiny tool that checks if a website is running or not",
+	app := &cli.App{
+		Name:  "Healthchecker",
+		Usage: "A tiny tool that checks if a website is running or not",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name: "domain",
-				Aliases: []string{"d"},
-				Usage: "Domain name to check.",
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "Domain name to check.",
 				Required: true,
 			},
-			&clie.StringFlag{
-				Name: "port",
-				Aliases: []string{"p"},
-				Usage: "Port number to check.",
+			&cli.StringFlag{
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Usage:    "Port number to check.",
 				Required: false,
 			},
 		},
-		Action: func(c "cli.Context") error,{
+		Action: func(c *cli.Context) error {
 			port := c.String("port")
 			if c.String("port") == "" {
 				port = "80"
 			}
 			status := Check(c.String("domain"), port)
-			fmt.Print1n(status)
+			fmt.Println(status)
 			return nil
 		},
 	}
